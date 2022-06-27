f760cc6b315181db70e6ba06faf5137490369e40
TusharKanhaiya <88088269+TusharKanhaiya@users.noreply.github.com>
2022-06-27 18:49:04 +0530

    Add files via upload

--- Graph/getConnectedComponents.java
+++ Graph/getConnectedComponents.java
@@ -1,8 +1,27 @@
+import static org.junit.Assert.fail;
+
 import java.util.*;
 
 
 public class getConnectedComponents {
 
+    static class Pair {
+        int v;
+        ArrayList<Integer> psf;
+        // int wt;
+        
+        Pair(int v,ArrayList<Integer> psf)
+        {
+            this.v = v;
+            this.psf = psf;
+            // this.wt = wt;
+        }
+
+        public void ToString()
+        {
+            System.out.println("v "+this.v+" ->psf "+this.psf);
+        }
+    }
     
 
    
@@ -10,8 +29,7 @@ public class getConnectedComponents {
         int src;
         int nbr;
         // int wt;
-
-
+        
         Edge(int src,int nbr)
         {
             this.src = src;
@@ -33,35 +51,99 @@ public class getConnectedComponents {
         for(int i=0;i<vrtx;i++)
             gr[i] = new ArrayList<>();
 
-        gr[0].add(new Edge(0,1));
-        gr[1].add(new Edge(1,0));
-        gr[2].add(new Edge(2,3));
-        gr[3].add(new Edge(3,2));
-        gr[4].add(new Edge(4,5));
-        gr[4].add(new Edge(4,6));
-        gr[5].add(new Edge(5,6));
-        gr[5].add(new Edge(5,4));
-        gr[6].add(new Edge(6,5));
-        gr[6].add(new Edge(6,4));
+            gr [0].add( new Edge ( 0 , 3 ) ) ;
+            gr [0].add( new Edge ( 0 , 1 ) ) ;
+            gr [1].add( new Edge ( 1 , 0 ) ) ;
+            gr [1].add( new Edge ( 1 , 2 ) ) ;
+    
+            gr [ 2 ] .add ( new Edge ( 2 , 3 ) ) ;
+            // gr [ 2 ] .add ( new Edge ( 2 , 5 ) ) ;
+            gr [ 2 ] .add ( new Edge ( 2 , 1 ) ) ;
+            gr [ 3 ] .add ( new Edge ( 3 , 0 ) ) ;
+            gr [ 3 ] .add ( new Edge ( 3 , 2 ) ) ;
+            gr [ 3 ] .add ( new Edge ( 3 , 4 ) ) ;
+    
+            
+            gr [ 4 ] .add ( new Edge ( 4 , 3  ) ) ;
+            gr [ 4 ] .add ( new Edge ( 4 , 5  ) ) ;
+            gr [ 4 ] .add ( new Edge ( 4 , 6  ) ) ;
+            gr [ 5 ] .add ( new Edge ( 5 , 4  ) ) ;
+            // gr [ 5 ] .add ( new Edge ( 5 , 2  ) ) ;
+            gr [ 5 ] .add ( new Edge ( 5 , 6  ) ) ;
+            gr [ 6 ] .add ( new Edge ( 6 , 5  ) ) ;
+            gr [ 6 ] .add ( new Edge ( 6 , 4  ) ) ;
 
         boolean visi[] = new boolean[vrtx];
 
-        ArrayList<ArrayList<Integer>> lls = new ArrayList<>();
-        for(int i=0;i<vrtx;i++)
+        ArrayDeque<Pair> dq = new ArrayDeque<>();
+        ArrayList<Integer> pls = new ArrayList<>();
+        pls.add(2);
+        dq.add(new Pair(2,pls));
+        
+        while(!dq.isEmpty())
         {
-            if(visi[i]==false)
+            Pair p = dq.pollFirst();
+        
+            System.out.println(p.psf);
+            for(Edge e:gr[p.v])
             {
-                ArrayList<Integer> ls = new ArrayList<>();
-                connected(gr,i,visi,ls);
-                lls.add(ls);
+                if(!(p.psf).contains(e.nbr))
+                {
+                    ArrayList<Integer> pls1 = new ArrayList<>();
+                    pls1.addAll(p.psf);
+                    pls1.add(e.nbr);
+                    dq.add(new Pair(e.nbr,pls1));
+                }
+                else
+                {
+                    if(p.psf.size()>=3 && e.nbr==p.psf.get(0))
+                        System.out.println(p.psf+" contains cycle");
+                }
             }
+
         }
-        for(ArrayList<Integer> l : lls)
-            System.out.println(l);
+
+
+
+
+        // int ct =0;
+        // getAllPaths(gr,0,vrtx-1,visi,""+0,ct);
+
+        // ArrayList<ArrayList<Integer>> lls = new ArrayList<>();
+        // for(int i=0;i<vrtx;i++)
+        // {
+        //     if(visi[i]==false)
+        //     {
+        //         ArrayList<Integer> ls = new ArrayList<>();
+        //         connected(gr,i,visi,ls);
+        //         lls.add(ls);
+        //     }
+        // }
+        // for(ArrayList<Integer> l : lls)
+        //     System.out.println(l);
 
     }
 
-    private static void connected(ArrayList<getConnectedComponents.Edge>[] gr, int i, boolean[] visi, ArrayList<Integer> ls) {
+    private static void getAllPaths(ArrayList<Edge>[] gr, int src, int dest, boolean[] visi,String psf,int ct) {
+        if(ct==6)
+        {
+            System.out.println(psf);
+            return;
+        }
+        visi[src]=true;
+        ct++;
+        for(Edge e:gr[src])
+        {
+            if(visi[e.nbr] == false)
+            {
+                getAllPaths(gr, e.nbr, dest, visi, psf+"->"+e.nbr,ct);
+            }
+        }
+        visi[src] = false;
+
+    }
+
+    private static void connected(ArrayList<Edge>[] gr, int i, boolean[] visi, ArrayList<Integer> ls) {
 
         visi[i] = true;
         ls.add(i);
